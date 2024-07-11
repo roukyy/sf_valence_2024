f8f899c3ae364693aeb9029ff9fe9c31b3ab57ef
roukyy <itsrouky@gmail.com>
2024-07-11 10:59:14 +0200

    feat(home): add link

--- src/Controller/Frontend/HomeController.php
+++ src/Controller/Frontend/HomeController.php
@@ -8,16 +8,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-#[Route('', name: ('app.home'))]
 class HomeController extends AbstractController
 {
-
-    public function __construct(
-        private EntityManagerInterface $em,
-    ) {
-    }
-
-    #[Route('', name: '.app.home', methods: ['GET'])]
+    #[Route('', name: 'app.home', methods: ['GET'])]
     public function index(ArticleRepository $repo): Response
     {
         return $this->render('Frontend/Home/index.html.twig', [
